44174b7dd55f82ebba41ca4d6da8a366b4c83eeb
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-06-13 13:24:09 +0530

    Login and sign up api is added

--- server/index.js
+++ server/index.js
@@ -2,12 +2,70 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const User = require("./models/user.moduls.js"); // ✅ use consistent naming
+const bcrypt = require('bcryptjs');
+require("dotenv").config();
 
 // Config
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+//Check user
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.find();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Register api
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userCreated = await User.create({ name, email, password: hashedPassword });
+
+    res.status(201).json({ message: "User is created", user: userCreated });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+//Signup api
+app.post("/signup", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log("Incoming data:", req.body);
+
+    if (!email || !password) {
+      return res.status(400).send("Email and password are required");
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return res.status(404).send("Email not found!");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).send("Wrong password");
+    }
+
+    res.status(200).json({ message: "Login successful", user });
+  } catch (error) {
+    console.error("Signup error:", error.message);
+    res.status(500).send("Something went wrong");
+  }
+});
 
 // Connect MongoDB
 mongoose.connect(process.env.MONGO_URL)
